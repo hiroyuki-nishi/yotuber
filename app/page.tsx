4671159d13861cf3ce8bf6e56d4188589932b101
nishi_hiroyuki <nishi_hiroyuki@cyberagent.co.jp>
2024-02-12 16:47:26 +0900

    UI調整

--- app/page.tsx
+++ app/page.tsx
@@ -5,28 +5,54 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+interface Thumnail {
+  url: string,
+  createdAt: string,
+  title: string,
+}
+
 export default function Home() {
-  const list = Array.from({length: 3}, (v, k) => k);
-  const card = (
-  <Card sx={{ width: 275 }}>
+  const thumnails: Thumnail[] = [
+    {
+      url: "http://img.youtube.com/vi/AuQov5W65LY/mqdefault.jpg",
+      createdAt: "2/12 09:00",
+      title: "マイクラバトミントンリーグ",
+    },
+    {
+      url: "http://img.youtube.com/vi/jHRlw1e3YEg/mqdefault.jpg",
+      createdAt: "2/11 18:00",
+      title: "逆転裁判4",
+    },
+    {
+      url: "http://img.youtube.com/vi/PFjnhRsJgHU/mqdefault.jpg",
+      createdAt: "2/10 21:00",
+      title: "私の酸素を吸わないでほしい ／ Vo.羽渦ミウネル",
+    },
+  ];
+
+  const cardComponent = (thumnail: Thumnail, key: number) => (
+    <Card key={key} sx={{ width: 300 }}>
       <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Title
-        </Typography>
-        <Typography variant="h5" component="div">
-          
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
+        <Typography className="font-bold mb-2 flex items-center">
+          <AccessTimeIcon />
+          <span className="ml-2">{thumnail.createdAt}</span>
         </Typography>
         <Typography variant="body2">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+          <img
+            src={`${thumnail.url}`}
+            alt="サムネイル"
+          />
+        </Typography>
+        <Typography className="font-bold mt-2 flex items-center">
+          <YouTubeIcon className="text-red-600"/>
+          <span className="ml-2">{thumnail.title}</span>
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Learn More</Button>
+        <Button size="small">xxx</Button>
       </CardActions>
     </Card>
   )
@@ -34,7 +60,7 @@ export default function Home() {
     <>
       <div className="h-screen w-screen flex justify-center items-center">
         <div className="flex">
-          {list.map((x, i) => card)}
+          {thumnails.map(cardComponent)}
         </div>
       </div>
     </>
